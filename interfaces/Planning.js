3f61b1b68a1283cfa80681080980f9133dc5c350
Altamore Quentin <quentin.altamore@estiam.com>
2019-02-27 00:47:27 +0100

    J'arrive pas à mettre en place le Calendrier, je ne comprends pas l'erreur indiquée

--- interfaces/Planning.js
+++ interfaces/Planning.js
@@ -1,5 +1,6 @@
 import React from 'react'
-import { View, Text } from 'react-native'
+import { View } from 'react-native'
+import { Calendar } from 'react-native-calendario'
 
 class Planning extends React.Component {
     constructor(props) {
@@ -8,7 +9,43 @@ class Planning extends React.Component {
 
     render() {
         return(
-            <View><Text>Mon Planning</Text></View>
+            <View>
+                {/*<Calendar
+                    onChange={range => console.log(range)}
+                    minDate="2019-01-01"
+                    startDate="2019-02-27"
+                    endDate="2019-12-31"
+                    theme={{
+                      weekColumnTextStyle: {
+                        color: 'red',
+                      },
+                      weekColumnStyle: {
+                        paddingVertical: 20,
+                      },
+                      weekColumnsContainerStyle: {
+                        backgroundColor: 'lightgrey',
+                      },
+                      monthTitleStyle: {
+                        color: 'blue',
+                      },
+                      nonTouchableDayContainerStyle: {
+                        backgroundColor: 'red',
+                      },
+                      nonTouchableDayTextStyle: {
+                        color: 'green',
+                      },
+                      dayTextStyle: {
+                        color: 'blue',
+                      },
+                      activeDayContainerStyle: {
+                        backgroundColor: 'lightgrey',
+                      },
+                      activeDayTextStyle: {
+                        color: 'red',
+                      },
+                    }}*/}
+                />
+            </View>
         )
     }
 }
